3321073ad01b702f2c1c87ce2269701ed1508b66
Khadiza98 <111932554+Khadiza98@users.noreply.github.com>
2022-08-23 22:59:24 -0700

    Add files via upload

---
+++ Hello World/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File: main.cpp
+ * Author: Khadiza Akter
+ * Created on August 23, 2022, 5:54 PM
+ * Purpose: displaying "Hello World".
+ */
+
+// System level libraries
+#include <iostream> // input-output library
+
+using namespace std;
+
+// Execution starts from here!
+int main(int argc, char** argv) {
+
+    // Display the outputs
+    cout << "Hello World" << endl;
+    
+    //Exit the program
+    return 0;
+    
+} // Close the main function
+
